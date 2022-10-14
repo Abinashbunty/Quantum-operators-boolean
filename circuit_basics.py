ea7c267613cfb3356a543d444b407a4199691138
Abinash Satapathy <iamabinash98@gmail.com>
2022-10-15 01:22:49 +0200

    Basics

---
+++ circuit_basics.py
@@ -0,0 +1,34 @@
+import numpy as np
+from qiskit import QuantumCircuit
+
+circ = QuantumCircuit(3)
+
+# Add a H gate on qubit 0, putting this qubit in superposition.
+circ.h(0)
+# Add a CX (CNOT) gate on control qubit 0 and target qubit 1, putting
+# the qubits in a Bell state.
+circ.cx(0, 1)
+# Add a CX (CNOT) gate on control qubit 0 and target qubit 2, putting
+# the qubits in a GHZ state.
+circ.cx(0, 2)
+
+circ.draw('mpl')
+
+from qiskit.quantum_info import Statevector
+
+# Set the intial state of the simulator to the ground state using from_int
+state = Statevector.from_int(0, 2**3)
+
+# Evolve the state by the quantum circuit
+state = state.evolve(circ)
+
+#draw using latex
+state.draw('latex')
+
+
+from qiskit.visualization import array_to_latex
+
+#Alternative way of representing in latex
+array_to_latex(state)
+
+state.draw('qsphere')
